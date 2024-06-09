ff5837d6ff028868c5906314c26446da577c1dc1
Krasovec97 <mitjakrasovec1@gmail.com>
2024-06-09 20:06:10 +0200

    Add setPostUrl to AccountTypeData in AccountTypeSelection
    Expanded the type AccountTypeData to include an optional setPostUrl field in AccountTypeSelection.tsx. This new field updates the PostUrl if it changes, adding more flexibility and maintainability to the code. Minor format adjustments were also made to increase code readability.

--- resources/js/Pages/Parts/FormParts/AccountTypeSelection.tsx
+++ resources/js/Pages/Parts/FormParts/AccountTypeSelection.tsx
@@ -2,14 +2,15 @@ import {useLaravelReactI18n} from "laravel-react-i18n";
 import FancyTitle from "../../../Components/FancyTitle";
 
 type AccountTypeData = {
-    is_business_account: boolean
+    is_business_account: boolean,
+    setPostUrl?: (value: (((prevState: string) => string) | string)) => void
 }
 
 type UserFormProps = AccountTypeData & {
     updateFields: (fields: Partial<AccountTypeData>, goToNextStep: boolean) => void
 }
 
-export function AccountTypeSelection({ updateFields, setPostUrl }: UserFormProps) {
+export function AccountTypeSelection({updateFields, setPostUrl}: UserFormProps) {
     const {t} = useLaravelReactI18n();
 
     let handleBusinessAccountClick = () => {
@@ -25,14 +26,16 @@ export function AccountTypeSelection({ updateFields, setPostUrl }: UserFormProps
 
     return (
         <>
-            <FancyTitle heading={t("Account type")} subtitle={t("Let's get started")} />
+            <FancyTitle heading={t("Account type")} subtitle={t("Let's get started")}/>
 
             <div className={"col-12 my-5 text-center"}>
-                <button type="button" className={"btn btn-lg btn-primary"} title={t("I'm offering work.")} onClick={() => handleBusinessAccountClick()}>
+                <button type="button" className={"btn btn-lg btn-primary"} title={t("I'm offering work.")}
+                        onClick={() => handleBusinessAccountClick()}>
                     {t("Business Account")}
                 </button>
 
-                <button type="button" className={"btn btn-lg btn-outline-primary ms-md-5 mt-4 mt-md-0"} title={t("I'm looking for work.")} onClick={() => handlePersonalAccountClick()}>
+                <button type="button" className={"btn btn-lg btn-outline-primary ms-md-5 mt-4 mt-md-0"}
+                        title={t("I'm looking for work.")} onClick={() => handlePersonalAccountClick()}>
                     {t("Personal Account")}
                 </button>
             </div>
