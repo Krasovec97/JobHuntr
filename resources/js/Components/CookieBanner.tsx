cebcca092d076d64427436289cb5add3f1efcedc
Krasovec97 <mitjakrasovec1@gmail.com>
2024-07-17 23:05:12 +0200

    Simplify CookieBanner component and update styles
    The CookieBanner component script has been simplified by removing unnecessary code blocks. The acceptCookies function covers the necessary functionality already. Also, the style resource has been updated to set a height for the offcanvas.

--- resources/js/Components/CookieBanner.tsx
+++ resources/js/Components/CookieBanner.tsx
@@ -3,24 +3,15 @@ import {useLaravelReactI18n} from "laravel-react-i18n";
 
 export default function CookieBanner() {
     const {t} = useLaravelReactI18n();
-    let cookieBanner = document.getElementById('cookieBanner');
-    if (cookieBanner !== null) {
-        cookieBanner.addEventListener('hidden.bs.offcanvas', function () {
-            localStorage.setItem("cookie_accepted", 'yes');
-        })
-    }
-
     const acceptCookies = () => localStorage.setItem("cookie_accepted", 'yes');
 
 
     return (
         <>
             <div className={"offcanvas offcanvas-bottom " + ((localStorage.getItem('cookie_accepted') !== null && localStorage.getItem('cookie_accepted') === 'yes') ? '' : 'show')} id="cookieBanner"
-                 aria-labelledby="cookieBannerLabel">
+                 aria-labelledby="cookieBannerLabel" data-bs-backdrop="static">
                 <div className="offcanvas-header">
                     <h5 className="offcanvas-title" id="cookieBannerLabel">{t("Cookies")}</h5>
-                    <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas"
-                            aria-label="Close"></button>
                 </div>
                 <div className="offcanvas-body small">
                     <div>
