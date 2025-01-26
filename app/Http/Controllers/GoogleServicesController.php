f0480a3d7ae8c57c6e1e936dbaa46e2be4099cc7
Krasovec97 <mitjakrasovec1@gmail.com>
2025-01-26 09:19:47 +0100

    Integrate Google Places API with backend and frontend.
    Added GoogleServicesController for handling autocomplete and place details API calls. Refactored frontend to use backend endpoints instead of directly calling Google Places API. Updated configuration to manage Google API key securely through environment variables.

---
+++ app/Http/Controllers/GoogleServicesController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Client\Response;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Validator;
+
+class GoogleServicesController extends Controller
+{
+    public function autoComplete(Request $request): Response|JsonResponse
+    {
+        $params = $request->json();
+
+        $validator = Validator::make($params->all(), [
+            'input' => ['required', 'string'],
+            'session' => ['required', 'string']
+        ]);
+
+        if ($validator->fails()) {
+            return new JsonResponse([
+                'error' => $validator->errors()->all()
+            ], 400);
+        }
+
+        $googleApiKey = config('jobhuntr.google.api_key');
+        $sessionId = $params->get('session');
+
+        return Http::post("https://places.googleapis.com/v1/places:autocomplete?key=$googleApiKey&sessionToken=$sessionId", [
+            'input' => $params->get('input')
+        ]);
+    }
+
+    public function getPlace(Request $request, string $placeId) {
+        $googleApiKey = config('jobhuntr.google.api_key');
+        return Http::get("https://places.googleapis.com/v1/places/$placeId?fields=addressComponents,location&key=$googleApiKey");
+    }
+}
