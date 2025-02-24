c057b12196e8d6fbbf80047dfe3f094a2aafc422
Krasovec97 <mitjakrasovec1@gmail.com>
2025-02-24 17:58:51 +0100

    Add SSR support with necessary configuration updates
    Implemented server-side rendering (SSR) for the application using Inertia.js and React. Updated `vite.config.js` and associated scripts to enable SSR, added an `ssr.jsx` entry point, and adjusted dependencies to support the new functionality. Minor fixes and adjustments were made to ensure compatibility and correct behavior.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -42,22 +42,22 @@
             /></noscript>
         <!-- End Meta Pixel Code -->
 
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-TRF524KEBJ"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-TRF524KEBJ');
+        </script>
+
         <!-- Scripts -->
         @routes
         @viteReactRefresh
         @vite(['resources/js/app.tsx', "resources/js/Pages/{$page['component']}.tsx"])
         @inertiaHead
     </head>
-
-    <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-TRF524KEBJ"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'G-TRF524KEBJ');
-    </script>
 
     <body class="font-sans antialiased">
         @inertia
