14587a816b293657ed65324e2f532123614d3aee
Krasovec97 <mitjakrasovec1@gmail.com>
2025-03-10 11:02:34 +0100

    Add user and company analytics widgets; update UI with fixes
    Introduced `UserGainedChart` and `UsersGainedOverview` widgets for user and job analytics. Enhanced job management functionality, including the ability to activate/deactivate job listings and enforce proper workflows. Improved UI consistency, added company details visibility, and corrected minor issues like localization, routing, and query adjustments.

--- resources/js/Pages/EmailVerified.tsx
+++ resources/js/Pages/EmailVerified.tsx
@@ -1,6 +1,7 @@
 import {useLaravelReactI18n} from "laravel-react-i18n";
 import React from "react";
 import MainLayout from "../Layouts/MainLayout";
+import {usePage} from "@inertiajs/react";
 
 
 export default function EmailVerified() {
@@ -12,6 +13,17 @@ export default function EmailVerified() {
                 <div className="mt-3">
                     <h1 className="fw-bold">{t("Your email has been verified!")}</h1>
                     <p>{t("You may now login and start using everything JobHuntr has to offer")}</p>
+                </div>
+
+                <div>
+                    <div className="row justify-content-center">
+                        <div className="col-6 text-end">
+                            <a className="btn btn-primary" href={`https://${usePage().props.business_url}/dashboard`}>{ t("Company login") }</a>
+                        </div>
+                        <div className="col-6 text-start">
+                            <a className="btn btn-outline-primary" href={'/login'}>{ t("Login") }</a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </MainLayout>
